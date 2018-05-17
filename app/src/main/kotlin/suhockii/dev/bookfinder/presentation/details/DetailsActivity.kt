49b02fe533fc0376f15932e9922d9dfbdd8ebe10
Maksim Sukhotski <suhockii@gmail.com>
2018-05-17 19:31:54 +0300

    Fix design

--- app/src/main/kotlin/suhockii/dev/bookfinder/presentation/details/DetailsActivity.kt
+++ app/src/main/kotlin/suhockii/dev/bookfinder/presentation/details/DetailsActivity.kt
@@ -5,13 +5,10 @@ import android.view.MenuItem
 import com.arellomobile.mvp.MvpAppCompatActivity
 import com.arellomobile.mvp.presenter.InjectPresenter
 import com.arellomobile.mvp.presenter.ProvidePresenter
-import com.squareup.picasso.Picasso
 import org.jetbrains.anko.setContentView
-import suhockii.dev.bookfinder.R
 import suhockii.dev.bookfinder.di.DI
 import suhockii.dev.bookfinder.di.module.DetailsActivityModule
 import suhockii.dev.bookfinder.domain.model.Book
-import suhockii.dev.bookfinder.openLink
 import suhockii.dev.bookfinder.presentation.books.BooksActivity
 import toothpick.Toothpick
 import javax.inject.Inject
@@ -38,7 +35,6 @@ class DetailsActivity : MvpAppCompatActivity(), DetailsView {
         val scope = Toothpick.openScopes(DI.APP_SCOPE, DI.DETAILS_ACTIVITY_SCOPE)
         Toothpick.inject(this@DetailsActivity, scope)
         layout.setContentView(this)
-        setSupportActionBar(layout.toolbar)
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
     }
 
@@ -53,20 +49,7 @@ class DetailsActivity : MvpAppCompatActivity(), DetailsView {
     }
 
     override fun showBookDetails(book: Book) = with(layout) {
-        Picasso.get().load(book.productLink).into(layout.ivBook)
         supportActionBar!!.title = book.shortName
-        fabBuy.setOnClickListener { openLink(book.website) }
-        tvFullName.text = book.fullName
-        tvPrice.text = getString(R.string.rubles, book.price)
-        tvISBN.text = book.productCode
-        tvAuthor.text = book.author
-        tvPublisher.text = book.publisher
-        tvYear.text = book.year
-        tvCover.text = book.cover
-        tvPageCount.text = book.pageCount
-        tvSeries.text = book.series
-        tvAvailability.text = book.status
-        tvDescription.text = book.description
     }
 }
 
