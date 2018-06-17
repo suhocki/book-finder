f6e1f1189f3793a5f3b13e207872fdacef8678f2
Maksim Sukhotski <suhockii@gmail.com>
2018-06-17 17:22:53 +0300

    Implement debug and release api key containers

--- app/src/main/kotlin/app/suhocki/mybooks/di/provider/InterstitialAdProvider.kt
+++ app/src/main/kotlin/app/suhocki/mybooks/di/provider/InterstitialAdProvider.kt
@@ -1,8 +1,7 @@
 package app.suhocki.mybooks.di.provider
 
 import android.content.Context
-import app.suhocki.mybooks.inDebug
-import app.suhocki.mybooks.inRelease
+import app.suhocki.mybooks.R
 import com.google.android.gms.ads.InterstitialAd
 import javax.inject.Inject
 import javax.inject.Provider
@@ -15,7 +14,6 @@ class InterstitialAdProvider @Inject constructor(
 
     override fun get(): InterstitialAd =
         InterstitialAd(context).apply {
-            inDebug { adUnitId = "ca-app-pub-3940256099942544/1033173712" }
-            inRelease { adUnitId = "ca-app-pub-5580850164009775/9559469093" }
+            adUnitId = context.getString(R.string.interstitial_ad_id)
         }
 }
