a338d3e2836517abe21d30c2b4c273e199200abc
Maksim Sukhotski <maksim.sukhotski@consultant.turkcell.com.tr>
2018-12-22 13:05:57 +0300

    Add original Terrakok Paginator from https://gitlab.com/terrakok/gitlab-client/blob/develop/app/src/main/java/ru/terrakok/gitlabclient/presentation/global/Paginator.kt

--- app/src/main/kotlin/app/suhocki/mybooks/presentation/base/paginator/Paginator.kt
+++ app/src/main/kotlin/app/suhocki/mybooks/presentation/base/paginator/Paginator.kt
@@ -1,22 +1,32 @@
 package app.suhocki.mybooks.presentation.base.paginator
 
-import app.suhocki.mybooks.presentation.base.paginator.state.*
-import app.suhocki.mybooks.uiThread
-import org.jetbrains.anko.doAsync
-import org.jetbrains.anko.uiThread
-import java.util.concurrent.Future
+import io.reactivex.Single
+import io.reactivex.disposables.Disposable
 
-
-class Paginator<T> constructor(
-    val view: PaginatorView<T>,
-    val listOfT: MutableList<T>,
-    private val requestFactory: (Int) -> List<T>
+/**
+ * Created by Konstantin Tskhovrebov (aka @terrakok) on 22.07.17.
+ */
+class Paginator<T>(
+    private val requestFactory: (Int) -> Single<List<T>>,
+    private val viewController: ViewController<T>
 ) {
 
-    var currentState: State<T> = Empty(this, view)
+    interface ViewController<T> {
+        fun showEmptyProgress(show: Boolean)
+        fun showEmptyError(show: Boolean, error: Throwable? = null)
+        fun showEmptyView(show: Boolean)
+        fun showData(show: Boolean, data: List<T> = emptyList())
+        fun showErrorMessage(error: Throwable)
+        fun showRefreshProgress(show: Boolean)
+        fun showPageProgress(show: Boolean)
+    }
 
-    internal var currentPage = FIRST_PAGE
-    internal var currentTask: Future<Unit>? = null
+    private val FIRST_PAGE = 1
+
+    private var currentState: State<T> = EMPTY()
+    private var currentPage = 0
+    private val currentData = mutableListOf<T>()
+    private var disposable: Disposable? = null
 
     fun restart() {
         currentState.restart()
@@ -34,34 +44,243 @@ class Paginator<T> constructor(
         currentState.release()
     }
 
-    internal fun loadPage(page: Int = FIRST_PAGE) {
-        doAsync({ throwable -> uiThread { currentState.fail(throwable) } }) {
-            val data = requestFactory.invoke(page)
-            uiThread {
-                currentState.newData(data)
+    private fun loadPage(page: Int) {
+        disposable?.dispose()
+        disposable = requestFactory.invoke(page)
+            .subscribe(
+                { currentState.newData(it) },
+                { currentState.fail(it) }
+            )
+    }
+
+    private interface State<T> {
+        fun restart() {}
+        fun refresh() {}
+        fun loadNewPage() {}
+        fun release() {}
+        fun newData(data: List<T>) {}
+        fun fail(error: Throwable) {}
+    }
+
+    private inner class EMPTY : State<T> {
+
+        override fun refresh() {
+            currentState = EMPTY_PROGRESS()
+            viewController.showEmptyProgress(true)
+            loadPage(FIRST_PAGE)
+        }
+
+        override fun release() {
+            currentState = RELEASED()
+            disposable?.dispose()
+        }
+    }
+
+    private inner class EMPTY_PROGRESS : State<T> {
+
+        override fun restart() {
+            loadPage(FIRST_PAGE)
+        }
+
+        override fun newData(data: List<T>) {
+            if (data.isNotEmpty()) {
+                currentState = DATA()
+                currentData.clear()
+                currentData.addAll(data)
+                currentPage = FIRST_PAGE
+                viewController.showData(true, currentData)
+                viewController.showEmptyProgress(false)
+            } else {
+                currentState = EMPTY_DATA()
+                viewController.showEmptyProgress(false)
+                viewController.showEmptyView(true)
             }
-        }.apply {
-            currentTask = this
+        }
+
+        override fun fail(error: Throwable) {
+            currentState = EMPTY_ERROR()
+            viewController.showEmptyProgress(false)
+            viewController.showEmptyError(true, error)
+        }
+
+        override fun release() {
+            currentState = RELEASED()
+            disposable?.dispose()
         }
     }
 
-    internal inline fun <reified T> toggleState() {
-        currentState = when (T::class.java) {
-            AllData::class.java -> AllData(this, view)
-            Data::class.java -> Data(this, view)
-            Empty::class.java -> Empty(this, view)
-            EmptyData::class.java -> EmptyData(this, view)
-            EmptyError::class.java -> EmptyError(this, view)
-            EmptyProgress::class.java -> EmptyProgress(this, view)
-            PageProgress::class.java -> PageProgress(this, view)
-            Refresh::class.java -> Refresh(this, view)
-            Released::class.java -> Released()
-            else -> throw NotImplementedError("Cannot determine state: ${T::class.java}")
+    private inner class EMPTY_ERROR : State<T> {
+
+        override fun restart() {
+            currentState = EMPTY_PROGRESS()
+            viewController.showEmptyError(false)
+            viewController.showEmptyProgress(true)
+            loadPage(FIRST_PAGE)
+        }
+
+        override fun refresh() {
+            currentState = EMPTY_PROGRESS()
+            viewController.showEmptyError(false)
+            viewController.showEmptyProgress(true)
+            loadPage(FIRST_PAGE)
+        }
+
+        override fun release() {
+            currentState = RELEASED()
+            disposable?.dispose()
         }
     }
 
-    companion object {
-        const val FIRST_PAGE = 1
-        const val LIMIT = 3
+    private inner class EMPTY_DATA : State<T> {
+
+        override fun restart() {
+            currentState = EMPTY_PROGRESS()
+            viewController.showEmptyView(false)
+            viewController.showEmptyProgress(true)
+            loadPage(FIRST_PAGE)
+        }
+
+        override fun refresh() {
+            currentState = EMPTY_PROGRESS()
+            viewController.showEmptyView(false)
+            viewController.showEmptyProgress(true)
+            loadPage(FIRST_PAGE)
+        }
+
+        override fun release() {
+            currentState = RELEASED()
+            disposable?.dispose()
+        }
     }
+
+    private inner class DATA : State<T> {
+
+        override fun restart() {
+            currentState = EMPTY_PROGRESS()
+            viewController.showData(false)
+            viewController.showEmptyProgress(true)
+            loadPage(FIRST_PAGE)
+        }
+
+        override fun refresh() {
+            currentState = REFRESH()
+            viewController.showRefreshProgress(true)
+            loadPage(FIRST_PAGE)
+        }
+
+        override fun loadNewPage() {
+            currentState = PAGE_PROGRESS()
+            viewController.showPageProgress(true)
+            loadPage(currentPage + 1)
+        }
+
+        override fun release() {
+            currentState = RELEASED()
+            disposable?.dispose()
+        }
+    }
+
+    private inner class REFRESH : State<T> {
+
+        override fun restart() {
+            currentState = EMPTY_PROGRESS()
+            viewController.showData(false)
+            viewController.showRefreshProgress(false)
+            viewController.showEmptyProgress(true)
+            loadPage(FIRST_PAGE)
+        }
+
+        override fun newData(data: List<T>) {
+            if (data.isNotEmpty()) {
+                currentState = DATA()
+                currentData.clear()
+                currentData.addAll(data)
+                currentPage = FIRST_PAGE
+                viewController.showRefreshProgress(false)
+                viewController.showData(true, currentData)
+            } else {
+                currentState = EMPTY_DATA()
+                currentData.clear()
+                viewController.showData(false)
+                viewController.showRefreshProgress(false)
+                viewController.showEmptyView(true)
+            }
+        }
+
+        override fun fail(error: Throwable) {
+            currentState = DATA()
+            viewController.showRefreshProgress(false)
+            viewController.showErrorMessage(error)
+        }
+
+        override fun release() {
+            currentState = RELEASED()
+            disposable?.dispose()
+        }
+    }
+
+    private inner class PAGE_PROGRESS : State<T> {
+
+        override fun restart() {
+            currentState = EMPTY_PROGRESS()
+            viewController.showData(false)
+            viewController.showPageProgress(false)
+            viewController.showEmptyProgress(true)
+            loadPage(FIRST_PAGE)
+        }
+
+        override fun newData(data: List<T>) {
+            if (data.isNotEmpty()) {
+                currentState = DATA()
+                currentData.addAll(data)
+                currentPage++
+                viewController.showPageProgress(false)
+                viewController.showData(true, currentData)
+            } else {
+                currentState = ALL_DATA()
+                viewController.showPageProgress(false)
+            }
+        }
+
+        override fun refresh() {
+            currentState = REFRESH()
+            viewController.showPageProgress(false)
+            viewController.showRefreshProgress(true)
+            loadPage(FIRST_PAGE)
+        }
+
+        override fun fail(error: Throwable) {
+            currentState = DATA()
+            viewController.showPageProgress(false)
+            viewController.showErrorMessage(error)
+        }
+
+        override fun release() {
+            currentState = RELEASED()
+            disposable?.dispose()
+        }
+    }
+
+    private inner class ALL_DATA : State<T> {
+
+        override fun restart() {
+            currentState = EMPTY_PROGRESS()
+            viewController.showData(false)
+            viewController.showEmptyProgress(true)
+            loadPage(FIRST_PAGE)
+        }
+
+        override fun refresh() {
+            currentState = REFRESH()
+            viewController.showRefreshProgress(true)
+            loadPage(FIRST_PAGE)
+        }
+
+        override fun release() {
+            currentState = RELEASED()
+            disposable?.dispose()
+        }
+    }
+
+    private inner class RELEASED : State<T>
 }
